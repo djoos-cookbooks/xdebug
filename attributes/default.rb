9b6ca8c379fd32f06d170fb5d166bf1c980901a1
David King <dking@xforty.com>
2012-01-19 22:22:04 -0500

    Initial commit - not ready yet

---
+++ attributes/default.rb
@@ -0,0 +1,66 @@
+# 
+# Author:: David King <dking@xforty.com>
+# Cookbook Name:: xdebug
+# Attributes:: default
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Pear search does not currently work with the preferred state. So we have to
+# specify a default version for now.  https://pear.php.net/bugs/bug.php?id=19138
+# TODO: implement preferred_state attribute and logic once pear bug is fixed
+
+default[:xdebug][:auto_trace] = 0
+default[:xdebug][:cli_color] = 0
+default[:xdebug][:collect_assignments] = 0
+default[:xdebug][:collect_includes] = 1
+default[:xdebug][:collect_params] = 0
+default[:xdebug][:collect_return] = 0
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
+default[:xdebug][:] = 
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "xforty technologies"
+maintainer_email "dking@xforty.com"
+license          "Apache 2.0"
+description      "Installs/Configures xdebug"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
