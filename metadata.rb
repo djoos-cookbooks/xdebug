5ebdea2dcdd4ccaec7e03019cfaf4e46ab8cf1dd
David Joos <david.joos@gmail.com>
2014-05-08 12:09:43 +0100

    metadata update(s)

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
-maintainer       "David King, xforty technologies"
-maintainer_email "dking@xforty.com"
-license          "Apache 2.0"
-description      "Installs/Configures xdebug"
+name 'xdebug'
+maintainer 'Escape Studios'
+maintainer_email 'dev@escapestudios.com'
+license 'MIT'
+description 'Installs/Configures xdebug'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version '0.0.1'
 
-depends          "php"
-suggests         "apache2"
-suggests         "nginx"
+%w( debian ubuntu redhat centos fedora scientific amazon windows smartos ).each do |os|
+  supports os
+end
+
+depends 'php'
